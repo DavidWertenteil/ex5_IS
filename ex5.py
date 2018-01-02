c2b31b6f2aebd59139742575ddc30808f40ffead
DavidWertenteil <davhadassah@gmail.com>
2018-01-02 18:26:24 +0200

    Werking!

--- ex5.py
+++ ex5.py
@@ -11,28 +11,29 @@ import time
 # socket.connect(("127.0.0.1", 1234))
 
 string = "Bad password!"
-test = ['0'] * 8
+test = ['/'] * 8
+# test[0] = 'N'
 msg = ''
 max_time = 1
 
 while string == "Bad password!":
     s = ''
     a=s.join(test)
-    socket = sck.socket(
-    sck.AF_INET, sck.SOCK_STREAM)
+    print(a)
+    socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
     socket.connect(("127.0.0.1", 1234))
     socket.recv(1024)
+    time.time()
     s_t = time.time()
     socket.send(a)
     strings = socket.recv(1024)
     e_t = time.time()
+    # print(abs(s_t - e_t))
+    if test[len(msg)] == '/':
+        max_time = abs(s_t - e_t) + 0.05
 
-    if test[len(msg)] == '0':
-        max_time = abs(s_t - e_t)
-    print(max_time,"               " ,s_t - e_t)
-    if max_time > abs(s_t - e_t):
+    if max_time < abs(s_t - e_t):
         msg += test[len(msg)]
-        print(test)
     else:
         test[len(msg)] = chr(ord(test[len(msg)]) + 1)
     socket.close()

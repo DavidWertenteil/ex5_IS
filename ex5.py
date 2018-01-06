c0a02a0f486c4b83d95ab991c76a3337d9ec114f
pc <pc@DESKTOP-7TQM1P4>
2018-01-06 23:04:08 +0200

    Add program

--- ex5.py
+++ ex5.py
@@ -1,17 +1,31 @@
-__author__ = 'davidwr'
+__author__ = 'David Wertenteil'
 import socket as sck
-
 import time
 
+BUFF_SIZE = 1024  # 4 KiB
+
+
+def recvall(sock):
+    """
+    Receive all data from the socket
+    :param sock: The socket
+    :return: String with the message
+    """
+    data = ''
+    chunk = b'b'
+    while chunk != b'':
+        chunk = sock.recv(BUFF_SIZE)
+        data += chunk.decode('utf-8')
+    return data
+
+
 # Set all 8 characters to be '/' that way we can check '0'
-password = ['/'] * 8
-string = "Bad password!"
-msg = ''
+check_pass = ['/'] * 8
+message = "Bad password!"
+password = ''
 max_time = 1
 
-while string == "Bad password!":
-    a = str.encode(''.join(password))
-    # print(a)
+while message == "Bad password!":
     # Declaring a socket
     socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
     # Connecting to server
@@ -20,21 +34,24 @@ while string == "Bad password!":
     socket.recv(1024)
 
     # Sending the current password
-    socket.send(a)
+    socket.send(str.encode(''.join(check_pass), 'utf-8'))
 
     # Check receiving time
-    s_t = time.time()
-    strings = socket.recv(1024).decode('utf-8')
-    e_t = time.time()
+    start_t = time.time()
+    message = recvall(socket)
+    end_t = time.time()
 
-    if password[len(msg)] == '/':
-        max_time = abs(s_t - e_t) + 0.05  # Margin of error
+    if check_pass[len(password)] == '/':  # Next character
+        max_time = abs(start_t - end_t) + 0.05  # Margin of error
 
-    if max_time < abs(s_t - e_t):
-        msg += password[len(msg)]
+    # Add digit to final password
+    if max_time < abs(start_t - end_t):
+        password += check_pass[len(password)]
     else:
-        password[len(msg)] = chr(ord(password[len(msg)]) + 1)
+        check_pass[len(password)] = chr(ord(check_pass[len(password)]) + 1)
     socket.close()
 
 # Print the flag
-print(strings)
+print(message)
+# print password
+print()
